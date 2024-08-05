177fcac9642b078e8066e172057da11c5d954077
Attila Neto <54605291+AttilaNeto@users.noreply.github.com>
2024-08-05 14:39:39 -0300

    :recycle: refactor: Transforma em um serviço a criação do electron

--- base-aspnet-electron/Program.cs
+++ base-aspnet-electron/Program.cs
@@ -1,5 +1,5 @@
+using base_aspnet_electron.Services;
 using ElectronNET.API;
-using ElectronNET.API.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseElectron(args);
@@ -7,6 +7,7 @@ builder.WebHost.UseElectron(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddElectron();
+builder.Services.AddSingleton<ElectronService>();
 
 var app = builder.Build();
 
@@ -21,38 +22,8 @@ app.MapFallbackToFile("index.html");
 
 if (HybridSupport.IsElectronActive)
 {
-    CreateWindow();
+    var electronWindowService = app.Services.GetRequiredService<ElectronService>();
+    await electronWindowService.CreateWindowAsync();
 }
 
 app.Run();
-
-async void CreateWindow()
-{
-    var clientAppUrl = builder.Configuration.GetValue<string>("ClientApp:Url");
-
-    if (!string.IsNullOrEmpty(clientAppUrl))
-    {
-        Console.WriteLine($"ClientApp está rodando em: {clientAppUrl}");
-    }
-    else
-    {
-        Console.WriteLine("CLIENT_APP_URL não está definida.");
-        Console.WriteLine($"{clientAppUrl}");
-    }
-
-    var windowOptions = new BrowserWindowOptions
-    {
-        Width = 800,
-        Height = 600,
-        Title = "Projeto Base Electron", // Alterar o título da janela
-        Icon = "caminho/para/seu/icone.png", // Alterar o ícone da janela
-        AutoHideMenuBar = true // Esconder o menu
-    };
-
-    var window = await Electron.WindowManager.CreateWindowAsync(windowOptions);
-
-    window.OnClosed += () =>
-    {
-        Electron.App.Quit();
-    };
-}
---
+++ base-aspnet-electron/Services/ElectronService.cs
@@ -0,0 +1,51 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+
+namespace base_aspnet_electron.Services
+{
+    public class ElectronService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHostEnvironment _environment;
+
+        public ElectronService(IConfiguration configuration, IHostEnvironment environment)
+        {
+            _configuration = configuration;
+            _environment = environment;
+        }
+
+        public async Task CreateWindowAsync()
+        {
+            var windowOptions = new BrowserWindowOptions
+            {
+                Width = 800,
+                Height = 600,
+                Title = "Projeto Base Electron",
+                Icon = "caminho/para/seu/icone.png",
+                AutoHideMenuBar = true
+            };
+
+            var window = await Electron.WindowManager.CreateWindowAsync(windowOptions);
+
+            if (_environment.IsDevelopment())
+            {
+                var clientAppUrl = _configuration.GetValue<string>("ClientApp:Url");
+
+                if (!string.IsNullOrEmpty(clientAppUrl))
+                {
+                    window.LoadURL(clientAppUrl);
+                }
+                else
+                {
+                    Console.WriteLine("CLIENT_APP_URL não está definida.");
+                    window.Close();
+                }
+            }
+
+            window.OnClosed += () =>
+            {
+                Electron.App.Quit();
+            };
+        }
+    }
+}
